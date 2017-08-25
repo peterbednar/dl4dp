6814a961f4eefc4105199df97d79e5963678bcb4
peterbednar <peterbednar@users.noreply.github.com>
2017-08-25 14:37:14 +0200

    Update io.py

--- src/dl4dp/io.py
+++ src/dl4dp/io.py
@@ -1,38 +1,78 @@
 from __future__ import print_function
 
 import codecs
+from collections import OrderedDict
 
 ID, FORM, LEMMA, UPOS, XPOS, FEATS, HEAD, DEPREL, DEPS, MISC = range(10)
 
-def read_conllu(filename):
+EMPTY = 0
+MULTIWORD = 1
+
+def isempty(token):
+    if isinstance(token, list):
+        token = token[ID]
+    return token[2] == EMPTY if isinstance(token, tuple) else False
+
+def ismultiword(token):
+    if isinstance(token, list):
+        token = token[ID]
+    return token[2] == MULTIWORD if isinstance(token, tuple) else False
+
+def read_conllu(filename, skip_empty=True, skip_multiword=True):
 
     def parse_token(line):
-        columns = line.split("\t")
+        fields = line.split("\t")
 
-        if "." in columns[ID]:
-            token_id, index = columns[ID].split(".")
-            id = (int(token_id), int(index), ".")
-        elif "-" in columns[ID]:
-            start, end = columns[ID].split("-")
-            id = (int(start), int(end), "-")
+        if "." in fields[ID]:
+            token_id, index = fields[ID].split(".")
+            id = (int(token_id), int(index), EMPTY)
+        elif "-" in fields[ID]:
+            start, end = fields[ID].split("-")
+            id = (int(start), int(end), MULTIWORD)
         else:
-            id = int(columns[ID])
-        columns[ID] = id
+            id = int(fields[ID])
+        fields[ID] = id
 
-        return columns
+        for f in [LEMMA, UPOS, XPOS, FEATS, HEAD, DEPREL, DEPS, MISC]:
+            if fields[f] == "_":
+                fields[f] = None
+
+        if fields[FEATS]:
+            fields[FEATS] = parse_feats(fields[FEATS])
+
+        if fields[HEAD]:
+            fields[HEAD] = int(fields[HEAD])
+
+        if fields[DEPS]:
+            fields[DEPS] = parse_deps(fields[DEPS])
+
+        return fields
+
+    def parse_feats(str):
+        feats = OrderedDict()
+        for key, value in [feat.split("=") for feat in str.split("|")]:
+            if "," in value:
+                value = value.split(",")
+            feats[key] = value
+        return feats
+
+    def parse_deps(str):
+        return list(map(lambda rel: (int(rel[0]), rel[1]), [rel.split(":") for rel in str.split("|")]))
 
     def parse_sentence(lines):
-        sentence = [parse_token(line) for line in lines]
+        sentence = []
+        for line in lines:
+            token = parse_token(line)
+            if skip_empty and isempty(token):
+                continue
+            if skip_multiword and ismultiword(token):
+                continue
+            sentence.append(token)
         return sentence
 
     lines = []
-    with codecs.open(filename, "r", "utf-8") as f:
-        while True:
-            line = f.readline()
-            if not line:
-                if len(lines) != 0:
-                    yield parse_sentence(lines)
-                break
+    with codecs.open(filename, "r", "utf-8") as fp:
+        for line in fp:
             line = line.rstrip("\r\n")
             if line.startswith("#"):
                 continue
@@ -42,6 +82,8 @@ def read_conllu(filename):
                     lines = []
                 continue
             lines.append(line)
+        if len(lines) != 0:
+            yield parse_sentence(lines)
 
 if __name__ == "__main__":
     for s in read_conllu("../../test/test1.conllu"):
