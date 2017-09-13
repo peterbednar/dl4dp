80c782d149c4d91983ecb3bba8e91fe1a5838666
peterbednar <peterbednar@users.noreply.github.com>
2017-09-13 16:26:44 +0200

    Update model.py

--- src/model.py
+++ src/model.py
@@ -28,14 +28,14 @@ class Embeddings(object):
         x = [dy.concatenate([_lookup(i, f) for f in range(num_feats)]) for i in range(num_tokens)]
         return x
 
-    def set_update(self, update):
-        self.update = update if isinstance(update, (tuple, list)) else [update] * len(self.lookup)
+    def disable_dropout():
+        self.set_dropout(0)
 
     def set_dropout(self, dropout):
         self.dropout = dropout if isinstance(dropout, (tuple, list)) else [dropout] * len(self.lookup)
     
-    def disable_dropout():
-        self.set_dropout(0)
+    def set_update(self, update):
+        self.update = update if isinstance(update, (tuple, list)) else [update] * len(self.lookup)
 
     def param_collection(self):
         return self.pc
