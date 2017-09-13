292425eb9807c082e42080092c2a6b81f20bd388
peterbednar <peterbednar@users.noreply.github.com>
2017-09-13 14:45:18 +0200

    Update model.py

--- src/model.py
+++ src/model.py
@@ -7,35 +7,54 @@ from word2vec import read_word2vec
 
 class Embeddings(object):
 
-    def __init__(self, model, dims):
+    def __init__(self, model, dims, dropout=0):
         self.pc = model.add_subcollection()
         self.lookup = [self.pc.add_lookup_parameters(dim) for dim in dims]
-        self.spec = tuple(dims)
+        self.dropout = dropout
+        self.spec = (dims, dropout)
 
     def __call__(self, tree):
         num_tokens, num_feats = tree.feats.shape
-        x = [dy.concatenate([self.lookup[f][tree.feats[i,f]] for f in range(num_feats)]) for i in range(num_tokens)]
+
+        def _lookup(i):
+            x_i = [self.lookup[f][tree.feats[i,f]] for f in range(num_feats)]
+            if isinstance(self.dropout, (tuple, list)):
+                x_i = [dy.dropout(x_i[i], p) if p > 0 else x_i[i] for i, p in enumerate(self.dropout)]
+                x_i = dy.concatenate(x_i)
+            else:
+                x_i = dy.concatenate(x_i)
+                if self.dropout > 0:
+                    x_i = dy.dropout(x_i, self.dropout)
+            return x_i
+
+        x = [_lookup(i) for i in range(num_tokens)]
         return x
+
+    def set_dropout(self, dropout):
+        self.dropout = dropout
+    
+    def disable_dropout():
+        self.dropout = 0
 
     def param_collection(self):
         return self.pc
 
     @staticmethod
-    def init_from_array(model, arrays):
-        embeddings = Embeddings(model, [a.shape for a in arrays])
+    def init_from_array(model, arrays, dropout=0):
+        embeddings = Embeddings(model, [a.shape for a in arrays], dropout)
         for param, a in zip(embeddings.lookup, arrays):
             param.init_from_array(a)
         return embeddings
 
     @staticmethod
-    def init_from_word2vec(model, basename, fields=(FORM, UPOS, FEATS)):
+    def init_from_word2vec(model, basename, fields=(FORM, UPOS, FEATS), dropout=0):
         wv = read_word2vec(basename, fields)
-        return Embeddings.init_from_array(model, wv)
+        return Embeddings.init_from_array(model, wv, dropout)
 
     @staticmethod
     def from_spec(spec, model):
-        dims = spec
-        return Embeddings(model, dims)
+        dims, dropout = spec
+        return Embeddings(model, dims, dropout)
 
 from utils import DepTree
 
