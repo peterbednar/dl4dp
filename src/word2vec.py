a099a2f674ef77a63eff18fbe65feb4ec989e42f
peterbednar <peterbednar@users.noreply.github.com>
2017-09-14 21:41:05 +0200

    Update word2vec.py

--- src/word2vec.py
+++ src/word2vec.py
@@ -3,14 +3,15 @@ from __future__ import print_function
 import codecs
 import numpy as np
 from argparse import ArgumentParser
-from utils import FORM, UPOS, FEATS, field_to_str, str_to_field, read_conllu, create_dictionary, create_index, write_index, read_index
+from utils import FORM, UPOS, FEATS, FIELD_TO_STR, STR_TO_FIELD
+from utils import read_conllu, create_dictionary, create_index, write_index, read_index
 
 UNKNOWN_TOKEN = u"__unknown__"
 NONE_TOKEN = u"__none__"
 
 VECTORS_FILENAME = "{0}_{1}_vectors.txt"
 
-class Tokens(object):
+class _Tokens(object):
 
     def __init__(self, filename, field, index):
         self.filename = filename
@@ -33,10 +34,10 @@ class Tokens(object):
 
 def _word2vec(index, args):
     for i, f in enumerate(args.fields):
-        print("building {0} vectors...".format(field_to_str[f].upper()))
-        tokens = Tokens(args.inputfile, f, index)
+        print("building {0} vectors...".format(FIELD_TO_STR[f].upper()), end=" ")
+        tokens = _Tokens(args.inputfile, f, index)
         model = Word2Vec(tokens, sg=1 if args.sg else 0, size=args.size[i], window=args.window, min_count=1, workers=4, seed=args.seed)
-        model.wv.save_word2vec_format(VECTORS_FILENAME.format(args.outbasename, field_to_str[f]))
+        model.wv.save_word2vec_format(VECTORS_FILENAME.format(args.outbasename, FIELD_TO_STR[f]))
         print("done")
 
 def read_word2vec(basename, fields=(FORM, UPOS, FEATS), index=None):
@@ -45,7 +46,7 @@ def read_word2vec(basename, fields=(FORM, UPOS, FEATS), index=None):
     
     vectors = []
     for f in fields:
-        with codecs.open(VECTORS_FILENAME.format(basename, field_to_str[f]), "r", "utf-8") as fp:
+        with codecs.open(VECTORS_FILENAME.format(basename, FIELD_TO_STR[f]), "r", "utf-8") as fp:
             num_tokens, size = (int(num) for num in fp.readline().split(" "))
             num_tokens = len(index[f]) + 1
             a = np.zeros((num_tokens, size))
@@ -72,14 +73,14 @@ def _parse_args():
     parser.add_argument("--seed", default=1, type=int)
 
     args = parser.parse_args()
-    args.fields = [str_to_field[f.lower()] for f in args.fields]
+    args.fields = [STR_TO_FIELD[f.lower()] for f in args.fields]
     return args
 
 if __name__ == "__main__":
     from gensim.models import Word2Vec
     args = _parse_args()
 
-    print("building index...")
+    print("building index...", end=" ")
     dic = create_dictionary(read_conllu(args.inputfile), fields=args.fields)
     index = create_index(dic, min_frequency=args.min_frequency)
     print("done")
