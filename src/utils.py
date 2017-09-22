370a1f5999fd0db6535bf1e41b42c0063da033b3
peterbednar <peterbednar@users.noreply.github.com>
2017-09-22 23:26:34 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,302 +1,175 @@
 from __future__ import print_function
 
-import codecs
-import heapq
-import numpy as np
-from collections import Counter, OrderedDict, namedtuple, defaultdict
-from functools import total_ordering
+import math
+import dynet as dy
+from utils import FORM, UPOS, FEATS
+from word2vec import read_word2vec
 
-ID, FORM, LEMMA, UPOS, XPOS, FEATS, HEAD, DEPREL, DEPS, MISC = range(10)
+class Embeddings(object):
 
-EMPTY = 0
-MULTIWORD = 1
+    def __init__(self, model, dims, dropout=0, update=True):
+        self.pc = model.add_subcollection()
+        self.lookup = [self.pc.add_lookup_parameters(dim) for dim in dims]
+        self.set_dropout(dropout)
+        self.set_update(update)
+        self.dim = sum([dim for (_,dim) in dims])
+        self.spec = (dims, dropout, update)
 
-FIELD_TO_STR = ["id", "form", "lemma", "upos", "xpos", "feats", "head", "deprel", "deps", "misc"]
-STR_TO_FIELD = {k : v for v, k in enumerate(FIELD_TO_STR)}
+    def __call__(self, feats):
 
-def isempty(token):
-    if isinstance(token, list):
-        token = token[ID]
-    return token[2] == EMPTY if isinstance(token, tuple) else False
+        def _lookup(i, f):
+            embds = dy.lookup(self.lookup[f], feats[i,f], update=self.update[f])
+            dropout = self.dropout[f]
+            if dropout > 0:
+                embds = dy.dropout(embds, dropout)
+            return embds
 
-def ismultiword(token):
-    if isinstance(token, list):
-        token = token[ID]
-    return token[2] == MULTIWORD if isinstance(token, tuple) else False
+        num_tokens, num_feats = feats.shape
+        x = [dy.concatenate([_lookup(i, f) for f in range(num_feats)]) for i in range(num_tokens)]
+        return x
+
+    def set_dropout(self, dropout):
+        self.dropout = dropout if isinstance(dropout, (tuple, list)) else [dropout] * len(self.lookup)
     
-def normalize_lower(field, value):
-    return value.lower() if field == FORM else value
+    def disable_dropout(self):
+        self.set_dropout(0)
 
-def read_conllu(filename, skip_empty=True, skip_multiword=True, parse_feats=False, parse_deps=False, normalize=normalize_lower):
+    def set_update(self, update):
+        self.update = update if isinstance(update, (tuple, list)) else [update] * len(self.lookup)
 
-    def _parse_sentence(lines):
-        sentence = []
-        for line in lines:
-            token = _parse_token(line)
-            if skip_empty and isempty(token):
-                continue
-            if skip_multiword and ismultiword(token):
-                continue
-            sentence.append(token)
-        return sentence
+    def param_collection(self):
+        return self.pc
 
-    def _parse_token(line):
-        fields = line.split("\t")
+    @staticmethod
+    def init_from_array(model, arrays, dropout=0, update=True):
+        embeddings = Embeddings(model, [a.shape for a in arrays], dropout, update)
+        for param, a in zip(embeddings.lookup, arrays):
+            param.init_from_array(a)
+        return embeddings
 
-        if "." in fields[ID]:
-            token_id, index = fields[ID].split(".")
-            id = (int(token_id), int(index), EMPTY)
-        elif "-" in fields[ID]:
-            start, end = fields[ID].split("-")
-            id = (int(start), int(end), MULTIWORD)
+    @staticmethod
+    def init_from_word2vec(model, basename, fields=(FORM, UPOS, FEATS), index=None, dropout=0, update=True):
+        wv = read_word2vec(basename, fields, index)
+        return Embeddings.init_from_array(model, wv, dropout, update)
+
+    @staticmethod
+    def from_spec(spec, model):
+        dims, dropout, update = spec
+        return Embeddings(model, dims, dropout, update)
+
+class Dense(object):
+
+    def __init__(self, model, input_dim, output_dim, act=dy.rectify, init_gain=math.sqrt(2.), ln=False):
+        self.pc = model.add_subcollection()
+        self.act = act
+        self.ln = ln
+        self.W = self.pc.add_parameters((output_dim, input_dim), init=dy.GlorotInitializer(gain=init_gain))
+        self.b = self.pc.add_parameters(output_dim, init=dy.ConstInitializer(0.))
+        if ln:
+            self.g = self.pc.add_parameters(output_dim, init=dy.ConstInitializer(1.))
+        self.spec = (input_dim, output_dim, act, init_gain, ln)
+
+    def __call__(self, x):
+        W = dy.parameter(self.W)
+        b = dy.parameter(self.b)
+        if self.ln:
+            g = dy.parameter(self.g)
+            y = dy.layer_norm(W * x, g, b)
+            return self.act(y)
         else:
-            id = int(fields[ID])
-        fields[ID] = id
-
-        for f in [LEMMA, UPOS, XPOS, FEATS, HEAD, DEPREL, DEPS, MISC]:
-            if fields[f] == "_":
-                fields[f] = None
-
-        if parse_feats and fields[FEATS]:
-            fields[FEATS] = _parse_feats(fields[FEATS])
-
-        if fields[HEAD]:
-            fields[HEAD] = int(fields[HEAD])
-
-        if parse_deps and fields[DEPS]:
-            fields[DEPS] = _parse_deps(fields[DEPS])
-
-        if normalize:
-            for f in [FORM, LEMMA]:
-                fields[f] = normalize(f, fields[f])
-        
-        return fields
-
-    def _parse_feats(str):
-        feats = OrderedDict()
-        for key, value in [feat.split("=") for feat in str.split("|")]:
-            if "," in value:
-                value = value.split(",")
-            feats[key] = value
-        return feats
-
-    def _parse_deps(str):
-        return list(map(lambda rel: (int(rel[0]), rel[1]), [rel.split(":") for rel in str.split("|")]))
-
-    lines = []
-    with codecs.open(filename, "r", "utf-8") as fp:
-        for line in fp:
-            line = line.rstrip("\r\n")
-            if line.startswith("#"):
-                continue
-            if not line:
-                if len(lines) != 0:
-                    yield _parse_sentence(lines)
-                    lines = []
-                continue
-            lines.append(line)
-        if len(lines) != 0:
-            yield _parse_sentence(lines)
-
-def create_dictionary(sentences, fields={FORM, LEMMA, UPOS, XPOS, FEATS, DEPREL}):
-    dic = {f: Counter() for f in fields}
-    for sentence in sentences:
-        for token in sentence:
-            for f in fields:
-                s = token[f]
-                dic[f][s] += 1
-    return dic
-
-def create_index(dic, min_frequency=1):
-    for f, c in dic.items():
-        ordered = c.most_common()
-        min_fq = min_frequency[f] if isinstance(min_frequency, (list, tuple, dict)) else min_frequency
-        for i, (s, fq) in enumerate(ordered):
-            if fq >= min_fq:
-                c[s] = i + 1
-            else:
-                del c[s]
-    return dic
-
-def create_inverse_index(index):
-    return {f: {v: k for k, v in c.items()} for f, c in index.items()}
-
-INDEX_FILENAME = "{0}_{1}_index.txt"
-
-_NONE_TOKEN = u"__none__"
-
-def write_index(basename, index, fields={FORM, UPOS, FEATS, DEPREL}):
-    index = create_inverse_index(index)
-    for f in fields:
-        c = index[f]
-        with codecs.open(INDEX_FILENAME.format(basename, FIELD_TO_STR[f]), "w", "utf-8") as fp:
-            for i in range(1, len(c) + 1):
-                token = c[i]
-                if token is None:
-                    token = _NONE_TOKEN
-                print(token, file=fp)
-
-def read_index(basename, fields={FORM, UPOS, FEATS, DEPREL}):
-    index = {}
-    for f in fields:
-        index[f] = Counter()
-        with codecs.open(INDEX_FILENAME.format(basename, FIELD_TO_STR[f]), "r", "utf-8") as fp:
-            i = 1
-            for line in fp:
-                token = line.rstrip("\r\n")
-                if token == _NONE_TOKEN:
-                    token = None
-                index[f][token] = i
-                i += 1
-    return index
-
-class DepTree(namedtuple("DepTree", "feats, heads, labels")):
-
-    def __new__(cls, num_tokens, num_feats=0):
-        return super(cls, DepTree).__new__(cls,
-                np.empty((num_tokens, num_feats), dtype=np.int) if num_feats > 0 else None,
-                np.full(num_tokens, -1, dtype=np.int),
-                np.full(num_tokens, -1, dtype=np.int))
-
-    def __len__(self):
-        return len(self.heads)
-
-def map_to_instance(sentence, index, fields=(FORM, UPOS, FEATS)):
-    num_tokens = len(sentence)
-    num_feats = len(fields)
-    tree = DepTree(num_tokens, num_feats)
-
-    for i, token in enumerate(sentence):
-        for j, f in enumerate(fields):
-            tree.feats[i][j] = index[f][token[f]]
-        tree.heads[i] = token[HEAD]
-        tree.labels[i] = index[DEPREL][token[DEPREL]]
-
-    return tree
-
-def map_to_instances(sentences, index, fields=(FORM, UPOS, FEATS)):
-    for sentence in sentences:
-        yield map_to_instance(sentence, index, fields)
-
-def max_branching(weights, branching=None):
-
-    def _push(queue, elm):
-        heapq.heappush(queue, elm)
+            y = dy.affine_transform([b, W, x])
+            return self.act(y)
     
-    def _pop(queue):
-        if len(queue) == 0:
-            return None
-        return heapq.heappop(queue)
+    def param_collection(self):
+        return self.pc
 
-    def _find_disjoint_sets(trees, elm):
-        if trees[elm] != elm:
-            trees[elm] = _find_disjoint_sets(trees, trees[elm])
-        return trees[elm]
+    @staticmethod
+    def from_spec(spec, model):
+        input_dim, output_dim, act, init_gain, ln = spec
+        return Dense(model, input_dim, output_dim, act, init_gain, ln)
 
-    def _union_disjoint_sets(trees, set1, set2):
-        trees[set2] = set1
+class MultiLayerPerceptron(object):
 
-    def _invert_max_branching(node, h, visited, inverted):
-        visited[node] = True
-        for v in h[node]:
-            if visited[v]:
-                continue
-            inverted[v - 1] = node
-            _invert_max_branching(v, h, visited, inverted)
+    def __init__(self, model, dims, act=dy.rectify, init_gain=math.sqrt(2.), ln=False, dropout=0):
+        self.pc = model.add_subcollection()
+        self.layers = []
+        for input_dim, output_dim in zip(dims, dims[1:]):
+            self.layers.append(Dense(self.pc, input_dim, output_dim, act, init_gain, ln))
+        self.dropout = dropout
+        self.spec = (dims, act, init_gain, ln, dropout)
 
-    nr, nc = weights.shape
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+            if self.dropout > 0:
+                x = dy.dropout(x, self.dropout)
+        return x
 
-    roots = list(range(1, nr))
-    rset = [0]
+    def set_dropout(self, dropout):
+        self.dropout = dropout
+    
+    def disable_dropout(self):
+        self.set_dropout(0)
 
-    q = np.empty(nr, dtype=np.object)
-    enter = np.empty(nr, dtype=np.object)
+    def param_collection(self):
+        return self.pc
 
-    min = np.arange(nr, dtype=np.int)
-    s = np.arange(nr, dtype=np.int)
-    w = np.arange(nr, dtype=np.int)
+    @staticmethod
+    def from_spec(spec, model):
+        dims, act, init_gain, ln, dropout = spec
+        return MultiLayerPerceptron(model, dims, act, init_gain, ln, dropout)
 
-    h = defaultdict(list)
+class BiLSTM(object):
 
-    for node in range(1, nr):
-        q[node] = []
-        for i in range(nr):
-            if i != node:
-                _push(q[node], _Edge(i, node, weights[i, node]))
+    def __init__(self, model, input_dim, hidden_dim, num_layers=1, input_dropout=0, output_dropout=0, ln=False):
+        self.pc = model.add_subcollection()
 
-    while roots:
-        scc_to = roots.pop()
-        max_in_edge = _pop(q[scc_to])
+        def _build_layer(input_dim, hidden_dim, rnn_builder=dy.VanillaLSTMBuilder):
+            f = rnn_builder(1, input_dim, hidden_dim / 2, self.pc, ln)
+            b = rnn_builder(1, input_dim, hidden_dim / 2, self.pc, ln)
+            return (f, b)
 
-        if max_in_edge is None:
-            rset.append(scc_to)
-            continue
+        self.BOS = self.pc.add_parameters(input_dim)
+        self.EOS = self.pc.add_parameters(input_dim)
+        self.ROOT = self.pc.add_parameters(input_dim)
 
-        scc_from = _find_disjoint_sets(s, max_in_edge.start)
-        if scc_from == scc_to:
-            roots.append(scc_to)
-            continue
+        self.layers = [_build_layer(input_dim, hidden_dim)]
+        for _ in range(num_layers - 1):
+            self.layers.append(_build_layer(hidden_dim, hidden_dim))
+        self.set_dropouts(input_dropout, output_dropout)
 
-        h[max_in_edge.start].append(max_in_edge.end)
+        self.spec = input_dim, hidden_dim, num_layers, input_dropout, output_dropout, ln
 
-        wss_from = _find_disjoint_sets(w, max_in_edge.start)
-        wss_to = _find_disjoint_sets(w, max_in_edge.end)
-        if wss_from != wss_to:
-            _union_disjoint_sets(w, wss_from, wss_to)
-            enter[scc_to] = max_in_edge
-            continue
+    def __call__(self, x):
+        x = [dy.parameter(self.BOS), dy.parameter(self.ROOT)] + x + [dy.parameter(self.EOS)]
+        h = self.transduce(x)
+        h[:] = h[1:-1]
+        return h
 
-        min_weight = np.inf
-        min_scc = -1
-        tmp = max_in_edge
-        while tmp is not None:
-            if tmp.weight < min_weight:
-                min_weight = tmp.weight
-                min_scc = _find_disjoint_sets(s, tmp.end)
-            tmp = enter[_find_disjoint_sets(s, tmp.start)]
+    def transduce(self, x):
+        for (f,b) in self.layers:
+            fs = f.initial_state().transduce(x)
+            bs = b.initial_state().transduce(reversed(x))
+            x = [dy.concatenate([f,b]) for f,b in zip(fs, reversed(bs))]
+        return x
 
-        inc = min_weight - max_in_edge.weight
-        for e in q[scc_to]:
-            e.weight += inc
+    def set_dropout(self, dropout):
+        self.set_dropouts(dropout, dropout)
 
-        min[scc_to] = min[min_scc]
+    def set_dropouts(self, input_dropout, output_dropout):
+        for (f,b) in self.layers:
+            f.set_dropouts(input_dropout, output_dropout)
+            b.set_dropouts(input_dropout, output_dropout)
+    
+    def disable_dropout(self):
+        for (f,b) in self.layers:
+            f.disable_dropout()
+            b.disable_dropout()
 
-        tmp = enter[scc_from]
-        while tmp is not None:
-            inc = min_weight - tmp.weight
-            tmp_scc_to = _find_disjoint_sets(s, tmp.end)
-            for e in q[tmp_scc_to]:
-                e.weight += inc
-                _push(q[scc_to], e)
+    def param_collection(self):
+        return self.pc
 
-            _union_disjoint_sets(s, scc_to, tmp_scc_to)
-            q[tmp_scc_to] = None
-
-            tmp = enter[_find_disjoint_sets(s, tmp.start)]
-
-        roots.append(scc_to)
-
-    visited = np.zeros(nr, dtype=np.bool)
-    if branching is None:
-        branching = np.zeros(nr - 1, dtype=np.int)
-    for scc in rset:
-        _invert_max_branching(min[scc], h, visited, branching)
-
-    return branching
-
-@total_ordering
-class _Edge(object):
-
-    def __init__(self, start, end, weight):
-        self.start = start
-        self.end = end
-        self.weight = weight
-
-    def __eq__(self, other):
-        return (self.weight, self.start, self.end) == (other.weight, other.start, other.end)
-
-    def __lt__(self, other):
-        return (-self.weight, self.start, self.end) < (-other.weight, other.start, other.end)
-
-    def __repr__(self):
-        return str((self.start, self.end, self.weight))
+    @staticmethod
+    def from_spec(spec, model):
+        input_dim, hidden_dim, num_layers, input_dropout, output_dropout, ln = spec
+        return BiLSTM(model, input_dim, hidden_dim, num_layers, input_dropout, output_dropout, ln)
