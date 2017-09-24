1552f3a5f4b4f4da1ab85f8f2dd419a14865e3f7
peterbednar <peterbednar@users.noreply.github.com>
2017-09-24 17:02:34 +0200

    Update dl4dp.py

--- src/dl4dp.py
+++ src/dl4dp.py
@@ -13,49 +13,60 @@ from utils import DepTree, map_to_instances, read_conllu, read_index
 def arc_loss(model, tree):
     h = model.transduce(tree.feats)
     scores = model.predict_arcs(h)
-    loss = [dy.hinge(sc, tree.heads[dep], 1.0) for dep, sc in enumerate(scores)]
+    loss = [_hinge(sc, tree.heads[dep]) for dep, sc in enumerate(scores)]
     return dy.esum(loss)
 
 def label_loss(model, tree):
     h = model.transduce(tree.feats)
     scores = model.predict_labels(tree.heads, h)
-    loss = [dy.hinge(sc, tree.labels[dep] - 1, 1.0) for dep, sc in enumerate(scores)]
+    loss = [_hinge(sc, tree.labels[dep] - 1) for dep, sc in enumerate(scores)]
     return dy.esum(loss)
+
+def _hinge(exprs, target, margin=1.0):
+    scores = exprs.value()
+    best_wrong = max([(i, sc) for i, sc in enumerate(scores) if i != target], key=lambda x: x[1])[0]
+    if scores[target] < scores[best_wrong] + margin:
+        return exprs[best_wrong] - exprs[target] + margin
+    else:
+        return dy.constant(0.0)
 
 def evaluate(model, validation_data):
     num_tokens = 0.
-    correct_uas = correct_las = 0.
+    correct_ua = correct_la = 0.
 
+    model.disable_dropout()
     for i, gold in enumerate(validation_data):
         num_tokens += len(gold)
         parsed = model.parse(gold.feats)
 
         for n in range(len(gold)):
             if parsed.heads[n] == gold.heads[n]:
-                correct_uas += 1.
+                correct_ua += 1.
                 if parsed.labels[n] == gold.labels[n]:
-                    correct_las += 1.
+                    correct_la += 1.
 
         if (i % 100) == 0:
             print(".", end="")
             sys.stdout.flush()
+    model.enable_dropout()
 
-    uas = correct_uas / num_tokens
-    las = correct_las / num_tokens
+    uas = correct_ua / num_tokens
+    las = correct_la / num_tokens
     print("\nUAS: {0:.4}, LAS: {1:.4}".format(uas, las))
-
 
 if __name__ == "__main__":
     basename = "../build/cs"
     index = read_index(basename)
     train_data = list(map_to_instances(read_conllu("../treebanks/train/cs/cs.conllu"), index))
+    train_data = list(train_data[:10])
 
     pc = dy.ParameterCollection()
     model = MLPParser(pc, basename="../build/cs")
+    model.enable_dropout()
     trainer = dy.AdamTrainer(pc)
 
     print("training examples: {0}".format(len(train_data)))
-    for epoch in range(100):
+    for epoch in range(10):
         random.shuffle(train_data)
 
         total_loss = 0
