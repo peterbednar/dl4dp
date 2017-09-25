f76b4d849db3982a5c52bb1e5e73b42c71f40532
peterbednar <peterbednar@users.noreply.github.com>
2017-09-25 22:19:25 +0200

    Update dl4dp.py

--- src/dl4dp.py
+++ src/dl4dp.py
@@ -13,22 +13,28 @@ from utils import DepTree, map_to_instances, read_conllu, read_index
 def arc_loss(model, tree):
     h = model.transduce(tree.feats)
     scores = model.predict_arcs(h)
-    loss = [_hinge(sc, tree.heads[dep]) for dep, sc in enumerate(scores)]
+    loss = [hinge_loss(sc, tree.heads[dep]) for dep, sc in enumerate(scores)]
     return dy.esum(loss)
 
 def label_loss(model, tree):
     h = model.transduce(tree.feats)
     scores = model.predict_labels(tree.heads, h)
-    loss = [_hinge(sc, tree.labels[dep] - 1) for dep, sc in enumerate(scores)]
+    loss = [hinge_loss(sc, tree.labels[dep] - 1) for dep, sc in enumerate(scores)]
     return dy.esum(loss)
 
-def _hinge(exprs, target, margin=1.0):
+def hinge_loss(exprs, target, margin=1.0):
     scores = exprs.value()
     best_wrong = max([(i, sc) for i, sc in enumerate(scores) if i != target], key=lambda x: x[1])[0]
     if scores[target] < scores[best_wrong] + margin:
         return exprs[best_wrong] - exprs[target] + margin
     else:
         return dy.zeros(1)
+
+def shuffled_stream(data):
+    while True:
+        random.shuffle(data)
+        for d in data:
+            yield d
 
 def evaluate(model, validation_data):
     num_tokens = 0.
@@ -55,6 +61,7 @@ def evaluate(model, validation_data):
     print("\nUAS: {0:.4}, LAS: {1:.4}".format(uas, las))
 
 if __name__ == "__main__":
+
     basename = "../build/cs"
     index = read_index(basename)
     train_data = list(map_to_instances(read_conllu("../treebanks/train/cs/cs.conllu"), index))
@@ -65,21 +72,42 @@ if __name__ == "__main__":
     model.enable_dropout()
     trainer = dy.AdamTrainer(pc)
 
-    print("training examples: {0}".format(len(train_data)))
-    for epoch in range(10):
-        random.shuffle(train_data)
+    print("training sentences: {0}, tokens: {1}".format(len(train_data), sum([len(tree) for tree in train_data])))
 
-        total_loss = 0
-        for i, tree in enumerate(train_data):
-            dy.renew_cg()
-            loss = arc_loss(model, tree)
+    batch_size = 50
+    max_steps = 1000
+
+    step = 0
+    total_loss = 0
+    batch_loss = []
+    batch_tokens = 0
+
+    dy.renew_cg()
+    for tree in shuffled_stream(train_data):
+
+        batch_loss.append(arc_loss(model, tree))
+        batch_loss.append(label_loss(model, tree))
+
+        batch_tokens += len(tree)
+        if batch_tokens >= batch_size:
+            loss = dy.esum(batch_loss) * (1.0 / batch_tokens)
             total_loss += loss.value()
             loss.backward()
             trainer.update()
 
-            if (i % 100) == 0:
+            dy.renew_cg()
+            batch_loss = []
+            batch_tokens = 0
+            step += 1
+
+            if (step % 100) == 0:
                 print(".", end="")
                 sys.stdout.flush()
 
-        print("\naverage loss: {0}".format(total_loss / len(train_data)))
-        evaluate(model, train_data)
+            if (step % 1000) == 0:
+                print("\naverage loss: {0}".format(total_loss / 1000))
+                evaluate(model, train_data)
+                total_loss = 0.0
+
+            if step >= max_steps:
+                break
