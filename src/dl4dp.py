a08bbcb8a7e84ab1191b6526fd3531a7b1f4953b
peterbednar <peterbednar@users.noreply.github.com>
2017-10-06 14:56:25 +0200

    Update dl4dp.py

--- src/dl4dp.py
+++ src/dl4dp.py
@@ -13,31 +13,15 @@ from models import MLPParser
 from utils import create_index, create_dictionary, FORM, XPOS, DEPREL
 from utils import DepTree, map_to_instances, read_conllu, shuffled_stream
 
-def hinge_arc_loss(tree, scores):
+def hinge_loss(scores, gold):
     error = 0
-    loss = []
-    for i in range(len(tree)):
-        scarray = scores[i].npvalue()
-        gold = tree.heads[i]
-        #  or j != (i+1)
-        best_wrong = max([(j, sc) for j, sc in enumerate(scarray) if j != gold], key=lambda x: x[1])[0]
-        if scarray[gold] < scarray[best_wrong]:
-            error += 1
-        if scarray[gold] < scarray[best_wrong] + 1.0:
-            loss.append(arc_scores[i][best_wrong] - arc_scores[i][gold] + 1.0)
-    return error, loss
-
-def hinge_label_loss(tree, scores):
-    error = 0
-    loss = []
-    for i in range(len(tree)):
-        scarray = scores[i].npvalue()
-        gold = tree.labels[i] - 1
-        best_wrong = max([(j, sc) for j, sc in enumerate(scarray) if j != gold], key=lambda x: x[1])[0]
-        if scarray[gold] < scarray[best_wrong]:
-            error += 1
-        if scarray[gold] < scarray[best_wrong] + 1.0:
-            loss.append(label_scores[i][best_wrong] - label_scores[i][gold] + 1.0)
+    loss = None
+    scarray = scores.npvalue()
+    best_wrong = max([(i, sc) for i, sc in enumerate(scarray) if i != gold], key=lambda x: x[1])[0]
+    if scarray[gold] < scarray[best_wrong]:
+        error = 1
+    if scarray[gold] < scarray[best_wrong] + 1.0:
+        loss = scores[best_wrong] - scores[gold] + 1.0
     return error, loss
 
 def validate(model, validation_data):
@@ -67,10 +51,14 @@ if __name__ == "__main__":
 
     basename = "../build/en"
     train_filename = "../treebanks/train/en/en.conllu"
+    validation_filename = "../treebanks/dev/en/en.conllu"
 
     index = create_index(create_dictionary(read_conllu(train_filename), (FORM, XPOS, DEPREL)))
-    train_data = list(map_to_instances(read_conllu("../treebanks/train/en/en.conllu"), index, (FORM, XPOS)))
-    validation_data = train_data
+    train_data = list(map_to_instances(read_conllu(train_filename), index, (FORM, XPOS)))
+    if validation_filename:
+        validation_data = list(map_to_instances(read_conllu(validation_filename), index, (FORM, XPOS)))
+    else:
+        validation_data = None
 
     embeddings_dims = [(len(index[FORM])+1, 100), (len(index[XPOS])+1, 25)]
     labels_dim = len(index[DEPREL])
@@ -81,6 +69,8 @@ if __name__ == "__main__":
     trainer = dy.AdamTrainer(pc)
 
     print("training sentences: {0}, tokens: {1}".format(len(train_data), sum([len(tree) for tree in train_data])))
+    if validation_data:
+        print("validation sentences: {0}, tokens: {1}".format(len(validation_data), sum([len(tree) for tree in validation_data])))
 
     step_loss = 0.0
     step_arc_error = 0.0
@@ -95,14 +85,18 @@ if __name__ == "__main__":
         h = model.transduce(example.feats)
 
         arc_scores = model.predict_arcs(h)
-        arc_error, arc_loss = hinge_arc_loss(example, arc_scores)
-        step_arc_error += arc_error
-        loss.extend(arc_loss)
+        for i in range(len(example)):
+            arc_error, arc_loss = hinge_loss(arc_scores[i], example.heads[i])
+            step_arc_error += arc_error
+            if arc_loss:
+                loss.append(arc_loss)
 
         label_scores = model.predict_labels(example.heads, h)
-        label_error, label_loss = hinge_label_loss(example, label_scores)
-        step_label_error += label_error
-        loss.extend(label_loss)
+        for i in range(len(example)):
+            label_error, label_loss = hinge_loss(label_scores[i], example.labels[i] - 1)
+            step_label_error += label_error
+            if label_loss:
+                loss.append(label_loss)
 
         if loss:
             loss = dy.esum(loss)
