cbaacab7c1c68fa280e847497677e57c3b431139
Peter Bednar <naivebayes@gmail.com>
2020-05-28 17:39:57 +0200

    Refactored batch_pipeline

--- dl4dp/__init__.py
+++ dl4dp/__init__.py
@@ -12,24 +12,37 @@ def preprocess():
     p.replace('form', r'[0-9]+|[0-9]+\.[0-9]+|[0-9]+[0-9,]+', '__number__')
     return p
 
-def pipeline(models, index, batch_size=100):
+def apply_model(batch, model, fields):
+    preds = model.parse(batch, unbind=True, device='cpu')
+    fields |= preds.keys()
+    for f, pred in preds.items():
+        for instance, value in zip(batch, pred):
+            instance[f] = value
+    return batch
+
+def batch_pipeline(models, index):
     inverse_index = create_inverse_index(index)
+
     p = pipe()
     p = p.map(lambda instance: copy.deepcopy(instance))
     p = p.pipe(preprocess())
     p = p.to_instance(index)
-    p = p.batch(batch_size)
-    for model in models:
-        p = p.map(apply_model(model))
-    p.flatten()
-    p.to_sentence(inverse_index)
-    return p
 
-def apply_model(model):
-    def _apply_model(batch):
-        preds = model.parse(batch, unbind=True, device='cpu')
-        for f, pred in preds.items():
-            for instance, value in zip(batch, pred):
-                instance[f] = value
-    return _apply_model
+    def _batch_pipeline(batch):
+        parsed_fields = set()
+
+        parsed = pipe(batch).pipe(p).collect()
+        for model in models:
+            parsed = apply_model(parsed, model, parsed_fields)
+        parsed = pipe(parsed).only_fields(parsed_fields).map_to_sentence(inverse_index).collect()
+
+        for sentence, parsed_sentence in zip(batch, parsed):
+            for i, parsed_word in enumerate(parsed_sentence):
+                word = sentence.get(i + 1)
+                word.update(parsed_word)
+
+    return _batch_pipeline
+
+def load_pipeline(name, pipeline='tagger,parser'):
+    pass
 
